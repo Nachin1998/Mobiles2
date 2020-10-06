15d540b9af5ab5034b24c4d3185e52c231482b30
Ignacio <Ignacio.f.l@hotmail.com>
2020-10-06 02:48:09 -0300

    General progress, game already looks good af

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public Player player;
     public Animator anim;
+
+    public float stage1StartTime;
+    public float stage2StartTime;
+    public float stage3StartTime;
+    public float gameEndTime;
+
+    public PostProcessVolume ppv;
     float gameTimer = 0;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,18 +26,24 @@ public class GameManager : MonoBehaviour
     {
         gameTimer += Time.deltaTime;
 
-        Debug.Log(gameTimer);
-        if(gameTimer >= 10f && gameTimer < 30f)
+        if(gameTimer >= stage1StartTime && gameTimer < stage2StartTime)
         {
             anim.SetBool("EasyRotationStart", true);
             anim.SetBool("EasyRotationLoop", true);
         }
 
-        if(gameTimer >= 30f && gameTimer < 45f)
+        if(gameTimer >= stage2StartTime && gameTimer < stage3StartTime)
         {
             anim.SetBool("EasyRotationEnd", true);
             anim.SetBool("HardRotationStart", true);
             anim.SetBool("HardRotationLoop", true);
+        }
+
+        if (gameTimer >= stage3StartTime && gameTimer < gameEndTime)
+        {
+            anim.SetBool("HardRotationEnd", true);
+            anim.SetBool("HardestRotationStart", true);
+            anim.SetBool("HardestRotationLoop", true);
         }
     }
 }
