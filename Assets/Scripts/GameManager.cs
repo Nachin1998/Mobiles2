efd120fb8266c4ca21dc9f31725e35e6e41a581f
Ignacio <Ignacio.f.l@hotmail.com>
2020-10-05 23:31:44 -0300

    Added a bunch of animations, looking great

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public Player player;
+    public Animator anim;
+    float gameTimer = 0;
     void Start()
     {
         
@@ -14,6 +16,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameTimer += Time.deltaTime;
+
+        Debug.Log(gameTimer);
+        if(gameTimer >= 10f && gameTimer < 30f)
+        {
+            anim.SetBool("EasyRotationStart", true);
+            anim.SetBool("EasyRotationLoop", true);
+        }
+
+        if(gameTimer >= 30f && gameTimer < 45f)
+        {
+            anim.SetBool("EasyRotationEnd", true);
+            anim.SetBool("HardRotationStart", true);
+            anim.SetBool("HardRotationLoop", true);
+        }
     }
 }
